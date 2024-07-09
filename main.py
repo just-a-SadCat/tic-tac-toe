f9b2183c307215138785c24d0b7451a896d3820b
just-a-SadCat <artur.majewski603@gmail.com>
2024-07-09 14:10:04 +0200

    second commit

--- main.py
+++ main.py
@@ -1,58 +1,88 @@
-board = [["[ ]","[ ]","[ ]"],["[ ]","[ ]","[ ]"],["[ ]","[ ]","[ ]"]]
 
-class Player():
-    player_id: int
-    name: str
+class Player:
+    def __init__(self, player_id, name):
+        self.player_id = player_id 
+        self.name = name
+        self._symbol = None
 
+    @property
+    def symbol(self):
+        if self._symbol == None:
+            raise LookupError("A symbol wasn't assinged to a player!")
+        return self._symbol
 
-class Room():
-    room_id: int
-    first_player_id: int
-    second_player_id: int
-
-
-def print_board(board: list[list[str]]):
-    for i in range(0, 3):
-        print(board[i][0], board[i][1], board[i][2])
-
-
-def check_victory(board: list[list[str]], x: int, y: int, symbol: str):
-    if board[x][0] == symbol and board[x][1] == symbol and board[x][2] == symbol:
-        print("Player wins, a full row!")
-        return True
-    if board[0][y] == symbol and board[1][y] == symbol and board[2][y] == symbol:
-        print("Player wins, a full column!")
-        return True
-    if board[0][0] == symbol and board[1][1] == symbol and board[2][2] == symbol:
-        print("Player wins, a full diagonal!")
-        return True
-    if board[0][2] == symbol and board[1][1] == symbol and board[2][0] == symbol:
-        print("Player wins, a full diagonal!")
-        return True
-    return False
+    @symbol.setter
+    def symbol(self, _symbol):
+        self._symbol = _symbol
 
 
-def change_active_player():
-    ...
+class Board:
+    def __init__(self):
+        self.fields = [["[ ]","[ ]","[ ]"],["[ ]","[ ]","[ ]"],["[ ]","[ ]","[ ]"]]
 
 
-def edit_field(board: list[list[str]], symbol: str):
-    x = int(input("Select row:"))
-    y = int(input("Select column: "))
+    def check_victory(self, x, y, symbol):
+        if self.fields[x][0] == symbol and self.fields[x][1] == symbol and self.fields[x][2] == symbol:
+            print("Player wins, a full row!")
+            return True
+        if self.fields[0][y] == symbol and self.fields[1][y] == symbol and self.fields[2][y] == symbol:
+            print("Player wins, a full column!")
+            return True
+        if self.fields[0][0] == symbol and self.fields[1][1] == symbol and self.fields[2][2] == symbol:
+            print("Player wins, a full diagonal!")
+            return True
+        if self.fields[0][2] == symbol and self.fields[1][1] == symbol and self.fields[2][0] == symbol:
+            print("Player wins, a full diagonal!")
+            return True
+        return False
 
-    if board[x-1][y-1] == "[ ]":
-        board[x-1][y-1] = f"[{symbol}]"
-    else:
-        print("This field is already taken!")
 
-    if check_victory(board, x - 1, y - 1, f"[{symbol}]"):
-        return True
-    return False
+    def edit_field(self, symbol):
+        x = int(input("Select row:"))
+        y = int(input("Select column: "))
+
+        if self.fields[x-1][y-1] == "[ ]":
+            self.fields[x-1][y-1] = f"[{symbol}]"
+        else:
+            print("This field is already taken!")
+
+        if self.check_victory(x - 1, y - 1, f"[{symbol}]"):
+            return True
+        return False
+    
+    
+    def print_board(self):
+        for i in range(0, 3):
+            print(self.fields[i][0], self.fields[i][1], self.fields[i][2])
 
 
-x = 0
-while x == 0:
-    print_board(board)
-    if edit_field(board, "X"):
-        x = 1
-print_board(board)
+class Room:
+    def __init__(self, room_id, board, first_player: Player, second_player: Player):
+        self.room_id = room_id
+        self.board = board
+        self.first_player = first_player
+        self.second_player = second_player
+
+    def assign_symbols(self):
+        self.first_player.symbol("X")
+        self.second_player.symbol("O")
+
+    def begin_game(self):
+        x = 0
+        while x == 0:
+            board.print_board()
+            if board.edit_field(self.first_player.symbol):
+                x = 1
+        board.print_board
+
+
+
+player1 = Player(420, "Andrew")
+player2 = Player(69, "Ashley")
+
+board = Board()
+
+room = Room(1, board, player1, player2)
+
+room.assign_symbols()
+room.begin_game()
