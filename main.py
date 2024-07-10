12c6c656e6d6a070b289c788dfd34f482ef1fe88
just-a-SadCat <artur.majewski603@gmail.com>
2024-07-10 14:26:36 +0200

    exceptions implemented,main rearranged

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+class IncorrectInput(Exception):
+    pass
+
+
+class InvalidPlay(Exception):
+    pass
+
 
 class Player:
     def __init__(self, player_id, name):
-        self.player_id = player_id 
+        self.player_id = player_id
         self.name = name
         self._symbol = None
 
@@ -18,75 +25,96 @@ class Player:
 
 class Board:
     def __init__(self):
-        self.fields = [["[ ]","[ ]","[ ]"],["[ ]","[ ]","[ ]"],["[ ]","[ ]","[ ]"]]
+        self.fields = [
+            ["[ ]", "[ ]", "[ ]"],
+            ["[ ]", "[ ]", "[ ]"],
+            ["[ ]", "[ ]", "[ ]"],
+        ]
 
+    def check_victory(self, player):
+        symbol = f"[{player.symbol}]"
 
-    def check_victory(self, symbol, player_name):
-        symbol = f"[{symbol}]"
-
-        for x in range (0, 3):
-            if self.fields[x][0] == symbol and self.fields[x][1] == symbol and self.fields[x][2] == symbol:
-                print(f"{player_name} wins, a full row!")
+        for x in range(0, 3):
+            if (
+                self.fields[x][0] == symbol
+                and self.fields[x][1] == symbol
+                and self.fields[x][2] == symbol
+            ):
+                print(f"{player.name} wins, a full row!")
                 return True
-            
+
         for y in range(0, 3):
-            if self.fields[0][y] == symbol and self.fields[1][y] == symbol and self.fields[2][y] == symbol:
-                print(f"{player_name} wins, a full column!")
+            if (
+                self.fields[0][y] == symbol
+                and self.fields[1][y] == symbol
+                and self.fields[2][y] == symbol
+            ):
+                print(f"{player.name} wins, a full column!")
                 return True
-            
-        if self.fields[0][0] == symbol and self.fields[1][1] == symbol and self.fields[2][2] == symbol:
-            print(f"{player_name} wins, a full diagonal!")
+
+        if (
+            self.fields[0][0] == symbol
+            and self.fields[1][1] == symbol
+            and self.fields[2][2] == symbol
+        ):
+            print(f"{player.name} wins, a full diagonal!")
             return True
-        
-        if self.fields[0][2] == symbol and self.fields[1][1] == symbol and self.fields[2][0] == symbol:
-            print(f"{player_name} wins, a full diagonal!")
+
+        if (
+            self.fields[0][2] == symbol
+            and self.fields[1][1] == symbol
+            and self.fields[2][0] == symbol
+        ):
+            print(f"{player.name} wins, a full diagonal!")
             return True
-        
+
         return False
 
     def check_stalemate(self):
         for x in range(0, 3):
-            for y in range (0, 3):
+            for y in range(0, 3):
                 if self.fields[x][y] == "[ ]":
                     return False
         return True
 
-
-    def edit_field(self, symbol):
+    def edit_field(self, symbol) -> None:
         try:
-            x = int(input("Select row: "))
-            y = int(input("Select column: "))
+            print("Select (Pick from 1 to 3): 1.row 2.column:")
+            x = int(input())
+            y = int(input())
         except ValueError:
-            print("Incorrect input, was not a number!")
+            raise IncorrectInput("The input was not a number")
 
-        try:
-            if x > 3 or x < 1 or y > 3 or y < 0:
-                print("Please pick a number from 1 to 3!!")
-                return False
+        if x > 3 or x < 1 or y > 3 or y < 0:
+            raise IncorrectInput(
+                "The input was a number not corresponding to any row/column"
+            )
 
-            if self.fields[x-1][y-1] == "[ ]":
-                self.fields[x-1][y-1] = f"[{symbol}]"
-            else:
-                print("This field is already taken!")
-                return False
-            return True
-        
-        except UnboundLocalError:
-            print("Input incorrect, try again!")
-            return False
-    
-    
+        if self.fields[x - 1][y - 1] == "[ ]":
+            self.fields[x - 1][y - 1] = f"[{symbol}]"
+        else:
+            raise InvalidPlay("A used field was chosen")
+
     def print_board(self):
         for i in range(0, 3):
             print(self.fields[i][0], self.fields[i][1], self.fields[i][2])
 
 
 class Room:
-    def __init__(self, room_id, board, first_player: Player, second_player: Player):
+    def __init__(self, room_id, first_player: Player, second_player: Player):
         self.room_id = room_id
-        self.board = board
         self.first_player = first_player
         self.second_player = second_player
+        self.board = Board()
+        self._active_player = first_player
+
+    @property
+    def active_player(self):
+        return self._active_player
+
+    @active_player.setter
+    def active_player(self, active_player):
+        self._active_player = active_player
 
     def assign_symbols(self):
         self.first_player.symbol = "X"
@@ -94,32 +122,44 @@ class Room:
 
     def swap_players(self, previous_player: Player):
         if previous_player is self.first_player:
-            return self.second_player
-        return self.first_player
+            self.active_player = self.second_player
+            return
+        self.active_player = self.first_player
 
-    def begin_game(self):
-        active_player: Player = self.first_player
-        while True:
-            print(f"Current player: {active_player.name}")
-            board.print_board()
-            move_result = board.edit_field(active_player.symbol)
-            if move_result:
-                if board.check_victory(active_player.symbol, active_player.name):
-                    board.print_board()
-                    break
-                active_player = self.swap_players(active_player)
-            if(board.check_stalemate()):
-                print("It's a stalemate!")
-                board.print_board()
-                break
-            
-                
+    def make_play(self, active_player):
+        self.board.edit_field(active_player.symbol)
+        self.swap_players(active_player)
+
+
+def main(room: Room):
+    while True:
+        active_player = room.active_player
+        print(f"Current player: {active_player.name}")
+        room.board.print_board()
+        move_result = False
+        while move_result is False:
+            try:
+                room.make_play(active_player)
+            except IncorrectInput:
+                print("Your input was incorrect, try again!")
+            except InvalidPlay:
+                print("That play is impossible, try again!")
+            else:
+                move_result = True
+
+        if room.board.check_victory(active_player):
+            room.board.print_board()
+            break
+        if room.board.check_stalemate():
+            print("It's a stalemate!")
+            room.board.print_board()
+            break
+
+
 player1 = Player(420, "Andrew")
 player2 = Player(69, "Ashley")
 
-board = Board()
-
-room = Room(1, board, player1, player2)
-
+room = Room(1, player1, player2)
 room.assign_symbols()
-room.begin_game()
+
+main(room)
