e9adb57976e3980cb3107657fff2a343ca5d764d
just-a-SadCat <artur.majewski603@gmail.com>
2024-07-15 10:01:10 +0200

    added template endpoints

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ from app.room import Room
 app = FastAPI()
 room = None
 player = Player(0, "Maks")
+active_player = None
 
 
 @app.post("/rooms", response_model=uuid.UUID)
@@ -18,7 +19,31 @@ async def create_room(player_id: Annotated[int, Body(embed=True)]) -> uuid.UUID:
     return room.room_id()
 
 
-@app.get("/rooms/players", response_model=Player)
+@app.post("/rooms/players", response_model=Player)
 async def add_player(player_id: Annotated[int, Body(embed=True)]) -> Player:
     room.add_player(player)
-    print(f"Dodano gracza {player.name}")
+    print(f"Added player: {player.name}")
+    return player.player_id()
+
+
+@app.get("/rooms/players", response_model=bool)
+async def start_game():
+    return room.is_full()
+
+
+@app.post("/rooms/board", response_model=None)
+async def start_turn():
+    global active_player
+    active_player = room.active_player
+    room.print_board()
+
+
+@app.post("/rooms/board", response_model=None)
+async def make_play(row: int, col: int):
+    print(f"Current player: {active_player.name}")
+    room.make_play(active_player, row, col)
+
+
+@app.get("/rooms/board", response_model=bool)
+async def check_board_state():
+    return room.check_board_state(active_player)
